8b75672b1a020e72021a1eb7c850df5ddcc335e6
Chathura Munasinghe <mactmunasinghe@students.nsbm.ac.lk>
2023-07-20 00:49:47 +0530

    Files added and nuget installed

---
+++ ServerlessRESTAzure/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerlessRESTAzure
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var keyVaultUrl = new Uri(Environment.GetEnvironmentVariable("KeyVaultUrl"));
+            var secretClient = new SecretClient(keyVaultUrl, new DefaultAzureCredential());
+            var sc = SecretClient.GetSecret("sql").Value.Value;
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(sc));
+
+        }
+    }
+}
